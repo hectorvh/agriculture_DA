acba3d827e5fee99c79a80283cf2dc1c976cf47f
hectorvh <valdes.h.hector@gmail.com>
2026-02-09 15:30:46 +0100

    Update .gitignore to include renv/local/ and remove NUTS_RG_20M_2024_3035.gpkg file

---
+++ R/install_quarto.R
@@ -0,0 +1,4 @@
+# Run once in R or RStudio to install the Quarto R package.
+# Then you can render QMD files with: quarto::quarto_render("data_cleaning.qmd")
+
+install.packages("quarto", repos = "https://cloud.r-project.org")
